6f3b1eeeb2fa0ea3b3af0f2634181ac3a70a9a39
Mathieu <mathieu.letyrant@gmail.com>
2016-07-25 10:49:13 +0200

    Add random userAgent

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var jsdom = require('jsdom'),
-    Q = require('q');
+    Q = require('q'),
+    randomUserAgent = require('random-useragent');
 
 module.exports = function (originalUrl, callback) {
     var deferred = Q.defer();
@@ -15,7 +16,7 @@ module.exports = function (originalUrl, callback) {
             'Accept': 'text/html',
             //'Accept-Encoding': 'gzip',
             'Accept-Language': 'en-US;q=0.6,en;q=0.4',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36'
+            'User-Agent': randomUserAgent.getRandom()
         },
         proxy: process.env.PROXY_HOST,
         tunnel: process.env.PROXY_HOST ? false : undefined,
